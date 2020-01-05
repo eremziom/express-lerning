8c37f6a05207b28a7ae8db02b0c9a3f7932e1416
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-01-05 11:47:45 +0100

    add layouts

--- server.js
+++ server.js
@@ -7,37 +7,30 @@ const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-})
-
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', {layout: 'dark'});
 });
 
 app.use((req, res) => {
